72e8a1c5be8d2fd77ecc9faee704068179d24d7c
Simontagbor <simontagbor360@gmail.com>
2022-12-04 13:41:42 -0800

    add script to print ASCII alphabets except e and q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-print_alphabt.py
+# Simon Tagbor Gabriel K <simontagbor360@gmail.com>
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    if chr(letter) not in  'eq':
+        print("{}".format(chr(letter)), end="")
