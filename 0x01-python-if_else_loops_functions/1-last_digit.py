0a4d1a2e6e6cd59a54d69c176603a33160b4899d
Simontagbor <simontagbor360@gmail.com>
2022-12-04 13:24:05 -0800

    add sript to asign random signed number to a variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digits = str(number)
-digit = int(digits[-1])
-
-if  digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
 elif digit == 0:
-    print(f"Last digit of {number} is {digit} and is 0")
-elif digit < 6 and not 0:
-    print(f"Last digit of {number} is {digit} and is less  than 6 and not 0")
+    print("0")
+else:
+    print("less than 6 and not 0")
