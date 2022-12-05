d3121d0925fb61e4627bc3e02b4e721644f39328
Simontagbor <simontagbor360@gmail.com>
2022-12-05 01:53:21 -0800

    add script to replace a list element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+# Tagbor Simon <simontagbor360@gmail.com>
+
+""" A function to replace a list element at a given index """
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 and idx > (len(my_list) - 1):
+        return (my_list)
+    else:
+        my_list[idx] = element
+    return my_list
