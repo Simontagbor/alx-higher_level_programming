f2ea0777ae188552790aea58c72ac1ef674da185
Simontagbor <simontagbor360@gmail.com>
2022-12-05 01:36:53 -0800

    add a fix to python script

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     else:
-        return my_list[idx]
+        return (my_list[idx])
