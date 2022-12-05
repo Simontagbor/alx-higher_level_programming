9a31894e9d4af673ad85721feba97151b05777d9
Simontagbor <simontagbor360@gmail.com>
2022-12-05 13:34:39 -0800

    add script to manipulate matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@
 def print_matrix_integer(matrix=[[]]):
     """ A function that returns integers from a list of lists """
     for i in range(len(matrix)):
-        for j  in range(len(matrix[i])):
+        for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end="")
             if j != (len(matrix[i]) - 1):
                 print(" ", end="")
