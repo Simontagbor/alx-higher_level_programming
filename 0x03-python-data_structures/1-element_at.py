f9759a79c5e28f319e22ee330d048c1af4970afd
Simontagbor <simontagbor360@gmail.com>
2022-12-05 00:06:53 -0800

    fix pep8 style error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,8 @@
 # Simon Tagbor Gabriel <simontagbor360@gmail.com>
 
 """pretrieve element at a given index"""
+
+
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
