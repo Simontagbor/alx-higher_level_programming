af2e10b90eee7f4d7ab109aa74589b29f27e1fab
Simontagbor <simontagbor360@gmail.com>
2022-12-06 01:43:56 -0800

    addrequried arguments to the str.format function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,10 +6,11 @@
 if __name__ == "__main__":
     """simple script to make use of functions"""
     from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(add(a, b)))
-    print("{:d} - {:d} = {:d}".format(sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
