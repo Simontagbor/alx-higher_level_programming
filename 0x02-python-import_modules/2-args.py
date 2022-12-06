9a4dd04202f591a09d519714f223dfa8af6ad253
Simontagbor <simontagbor360@gmail.com>
2022-12-06 02:51:49 -0800

    fix bugs inside output strings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,12 @@
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) <= 1:
-        print("{:d}: argument".format(len(sys.argv) - 1))
-    else:
-        print("{:d}: arguments".format(len(sys.argv) - 1))
 
+    if (len(sys.argv) - 1) == 0:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    elif (len(sys.argv) - 1) == 1:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(len(sys.argv) - 1):
         print("{:d}: {}".format(i + 1, sys.argv[i+1]))
