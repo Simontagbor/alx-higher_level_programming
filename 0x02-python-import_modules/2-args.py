e2e98d0992a4e201b95689a377e13fb033cbb63a
Simontagbor <simontagbor360@gmail.com>
2022-12-06 02:36:29 -0800

    fix wrong output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
     else:
         print("{:d}: arguments".format(len(sys.argv) - 1))
 
-    for i in range(len(sys.argv)):
-        if i > 0:
-            print("{:d}: {}".format(i, sys.argv[i]))
+    for i in range(len(sys.argv) - 1):
+        print("{:d}: {}".format(i + 1, sys.argv[i+1]))
