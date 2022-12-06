f3a4aa5550a45d666c81c701ae0b702ee6ec9a56
Simontagbor <simontagbor360@gmail.com>
2022-12-06 00:45:33 -0800

    add scrip to print variables

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0-add.py
+# Simon Tagbor <simontagbor360@gmail.com>
+
+if __name__ == "__main__":
+    """Print the sum  of variables"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
