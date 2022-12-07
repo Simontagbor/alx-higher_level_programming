f05e5312c9bccfca0b4cdbfbbb0b336682f95918
Simontagbor <simontagbor360@gmail.com>
2022-12-07 13:31:02 -0800

    add aprogram to return a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# simontagbor  <simontagbor360@gmail.com>
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
