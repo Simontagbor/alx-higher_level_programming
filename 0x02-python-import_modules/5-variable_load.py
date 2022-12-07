e21ccedf1149d0e31661fff49778d242a63dd802
Simontagbor <simontagbor360@gmail.com>
2022-12-07 01:38:03 -0800

    add aprogram to load a variable from a python file

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-varaible_load.py
+# Simon Tagbor <simontagbor360@gmail.com>
+
+if __name__ == "__main__":
+    """a simple program to load a variable  """
+    from variable_load_5 import a
+    print("{:d}".format(a))
