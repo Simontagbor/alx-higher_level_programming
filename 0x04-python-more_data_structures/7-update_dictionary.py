fc8056166dde6a9b89c13262acf828aed0cd7f75
Simontagbor <simontagbor360@gmail.com>
2022-12-07 20:23:12 -0800

    add a program to replace or add value pairs in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Simon Tagbor <simontagbor360@gmail.com>
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
