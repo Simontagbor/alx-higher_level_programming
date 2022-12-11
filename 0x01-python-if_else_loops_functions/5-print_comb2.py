910fc6005e03b44d6a355db58b0be0f59d1aa315
Simontagbor <simontagbor360@gmail.com>
2022-12-11 09:39:06 -0800

    add program to print two digit integers in Ascending order

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-print_comb2.py
+# Simon Tagbor <simontagbor360@gmail.com>
+
+"""Print Numbers in Ascending order"""
+for num in range(0, 100):
+    if num == 99:
+        print("{:02d}".format(num))
+    else:
+        print("{:02d}".format(num), end=", ")
