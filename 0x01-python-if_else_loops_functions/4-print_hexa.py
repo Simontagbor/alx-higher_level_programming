627abbe8c4bbba7028b27492adc2e5f3c38ab51f
Simontagbor <simontagbor360@gmail.com>
2022-12-11 09:19:31 -0800

    add a program that prints a number and its corresponding hex value

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 4-print_hexa.py
+# Simon Tagbor <simontagbor360@gmail.com>
+
+"""print numbers 0 to 98 in decimal and hexadecimal"""
+for num in range(0, 99):
+    print("{} = {}".format(num, hex(num)))
