2412f132f3d7b24d54741d87e8e3e7bae7aa0e57
Simontagbor <simontagbor360@gmail.com>
2022-12-11 09:51:08 -0800

    fix a bug in program

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # 5-print_comb2.py
 # Simon Tagbor <simontagbor360@gmail.com>
-
-"""Print Numbers in Ascending order"""
-for num in range(0, 100):
-    if num == 99:
-        print("{}".format(num))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
     else:
-        print("{:02}".format(num), end=", ")
+        print("{:02}".format(number), end=", ")
