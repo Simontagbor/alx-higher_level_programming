3f1dabc831480ce47de1d0f56ada1d258a15b034
Simontagbor <simontagbor360@gmail.com>
2022-12-21 21:12:43 -0800

    add program to raise exception message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Simon Tagbor  <simontagbor360@gmail.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
