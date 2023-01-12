cff888fb0a1638dd0eadca0db39407e17a83232b
Simontagbor <simontagbor360@gmail.com>
2023-01-11 19:24:09 -0800

    add a class method to base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Simon Tagbor
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
