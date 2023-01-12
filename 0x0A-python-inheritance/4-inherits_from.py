f34ae8c079cdde4e2c9f47b9b493eb6924af16f0
Simontagbor <simontagbor360@gmail.com>
2023-01-11 19:03:38 -0800

    Add a function for checking if an object is an inherited instance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,5 +16,5 @@ def inherits_from(obj, a_class):
         Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-         return True
+        return True
     return False
