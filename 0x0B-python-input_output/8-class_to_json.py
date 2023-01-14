609825b7dbad5b09b7422ed2cc905ed2ee3c4c40
Simontagbor <simontagbor360@gmail.com>
2023-01-14 10:59:24 -0800

    a function that returns the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 8-class_to_json
+# Simon Tagbor <simontagbor360@gmail.com>
+"""Serialize the class description of a class instance"""
+
+def class_to_json(obj):
+    """Create dictionary containing the class description of an instance"""
+     return obj.__dir__
