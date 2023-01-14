4eb5161b65ba9ab85d92b1f9dcbf2ea39dc9c3cc
Simontagbor <simontagbor360@gmail.com>
2023-01-14 11:01:20 -0800

    Fix pycodestyle error

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,6 +3,7 @@
 # Simon Tagbor <simontagbor360@gmail.com>
 """Serialize the class description of a class instance"""
 
+
 def class_to_json(obj):
     """Create dictionary containing the class description of an instance"""
-     return obj.__dir__
+    return obj.__dir__
