9aaec9bf67f9e416bc3a39c57d791c0cb6b0e918
Simontagbor <simontagbor360@gmail.com>
2023-01-14 10:14:39 -0800

    add a program to create a list of command line arguments and save them to a json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 7-add_item.py
+# Simon  Tagbor <simontagbor360@gmail.com>
+""" create a list from all arguments """
+
+
+import sys
+import json
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    arguments = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arguments = []
+arguments = [val for val in sys.argv[1:]]
+save_to_json_file(arguments, "add_item.json")
