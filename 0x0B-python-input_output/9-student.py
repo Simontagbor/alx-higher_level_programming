8ad3688712c7c5cfd68f254ea76535c064d01876
Simontagbor <simontagbor360@gmail.com>
2023-01-14 11:22:36 -0800

    add a program to return the dictionary description of Student instances

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,18 +10,8 @@ class Student(object):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
+
         """retrieve the dictionary representation of a Student instance"""
         return self.__dict__
-
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
-
-
