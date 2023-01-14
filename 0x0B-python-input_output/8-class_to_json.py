b9e9cfa7993991d9de9ef2b4001a42298538ca47
Simontagbor <simontagbor360@gmail.com>
2023-01-14 11:06:17 -0800

    call the appropriate method of the instance

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 8-class_to_json
 # Simon Tagbor <simontagbor360@gmail.com>
-"""Serialize the class description of a class instance"""
+"""return the class description of a class instance"""
 
 
 def class_to_json(obj):
     """Create dictionary containing the class description of an instance"""
-    return obj.__dir__
+    return obj.__dict__
