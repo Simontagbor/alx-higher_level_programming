0702fbbe8dd1feecea8868f50c8122e913d688e0
Simontagbor <simontagbor360@gmail.com>
2023-01-16 00:34:57 -0800

    add public method to return the area of a rectangle instance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = y
+
+    def area(self):
+        """define public area property for rectangle """
+        return (self.width * self.height)
