8282d7a02ac97be33e29318943acb699387a70cc
Simontagbor <simontagbor360@gmail.com>
2023-01-16 00:28:34 -0800

    add private rectangle attributes width, height, x cordinate, y cordinate. and define getter and setter methods for each

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,20 +9,68 @@ from models.base import Base
 class Rectangle(Base):
     """define constructor"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.set_width(width)
-        self.set_height(height)
-        self.set_x(x)
-        self.set_y(y)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
-    def set_width(self, width):
-        self.__width = width
+    @property
+    def width(self):
+        """set or get with attribute"""
+        return self.__width
 
-    def set_height(self, height):
-        self.__height = height
+    @width.setter
+    def width(self, width):
+        """Add validation"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
 
-    def set_x(self, x):
-        self._x = x
+    @property
+    def height(self):
+        """set or get with attribute"""
+        return self.__height
 
-    def set_y(self, y):
-        self.__y = y
+    @height.setter
+    def height(self, height):
+        """Add validation"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+
+    @property
+    def x(self):
+        """set or get x cordinatate of rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """Add Validation"""
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
+
+    @property
+    def y(self):
+        """set or get y cordinate of ractangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """Add Validation"""
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y
