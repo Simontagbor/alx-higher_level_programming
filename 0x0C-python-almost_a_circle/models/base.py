3f6bdafc002357f9d747bf9629819660e3fcfbc2
Simontagbor <simontagbor360@gmail.com>
2023-01-20 06:41:22 -0800

    Add method to save list of  Instances to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,25 @@ class Base(object):
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns a JSON string of list of dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns a list from json string"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save json string to file"""
+        filename =  "{}.json".format(cls.__name__)
+
+        with open(filename, "w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write(json.loads("[]"))
+            else:
+                dicts = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(dicts))
