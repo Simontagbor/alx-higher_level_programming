fa0e0f9bad8500e7d3d61acfbd0bafb6c483843a
Simontagbor <simontagbor360@gmail.com>
2023-01-20 03:43:51 -0800

    add static method to the base class to serialize a list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@
 """This  module implements a base class"""
 
 
+import json
+
+
 class Base(object):
     """set private variables"""
     __nb_objects = 0
@@ -15,3 +18,10 @@ class Base(object):
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns a JSON string of list of dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
