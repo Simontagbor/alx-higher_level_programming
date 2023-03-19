d3fa15e231451e9b7675fce628c36c0a69ddea90
Simon Tagbor <simontagbor360@gmail.com>
2023-03-19 05:07:03 -0700

    fix pycoestyle error and sort results in Ascending order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # List every state from the database hbtn_0e_0_usa
-# Script takes 3 arguments, the mysql username, mysql password and database name
+# Script takes 3 arguments: mysql username,password and databas
 # Usage ./0-select_states.py [mysql username] [password] [database name]
 
 import sys
@@ -9,5 +9,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
+    c.execute("SELECT * FROM `states` ORDER BY states.id ASC")
     [print(state) for state in c.fetchall()]
