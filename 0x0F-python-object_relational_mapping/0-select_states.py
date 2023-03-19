680b94bcc0f6df118686f8af2a7b40973552071e
Simon Tagbor <simontagbor360@gmail.com>
2023-03-19 04:43:10 -0700

    add script tot print the result of sql queries using MySQLdb database connector

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# List every state from the database hbtn_0e_0_usa
+# Script takes 3 arguments, the mysql username, mysql password and database name
+# Usage ./0-select_states.py [mysql username] [password] [database name]
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+working  with Object relational mappers
