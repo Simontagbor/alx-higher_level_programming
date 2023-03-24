c39f3014e990e56b40a0f4f077ff6bb90c05ca52
Simon Tagbor <simontagbor360@gmail.com>
2023-03-24 13:48:34 -0700

    remove sort by option in the selet statement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     """list all states from the database hbtn_0e_0_usa"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY states.id ASC")
+    c.execute("SELECT * FROM  `states`")
     [print(state) for state in c.fetchall()]
