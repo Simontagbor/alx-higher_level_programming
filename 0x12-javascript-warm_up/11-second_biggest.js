b8220baed596d40115a50f668e1a7c26f72f2bbc
Simon Tagbor <simontagbor360@gmail.com>
2023-06-19 15:41:26 -0700

    update test for  array length

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const args = process.argv;
 const arr = Object.values(args).slice(2);
 
 function findSecondBiggest (arr) {
-  if (arr.length <= 3) {
+  if (arr.length <= 1) {
     return 0;
   } else {
     return parseInt(arr.sort()[arr.length - 2]);
