d5defb729f52060322fa4f0ed3812b2d713da57b
Simon Tagbor <simontagbor360@gmail.com>
2023-06-19 23:54:17 -0700

    fix bug caused by sort() sorting alphabetically

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 const args = process.argv;
 const arr = Object.values(args).slice(2);
+const numbers = arr.map(str => {
+    return parseInt(str, 10);
+});
 
 function findSecondBiggest (arr) {
   if (arr.length <= 1) {
     return 0;
   } else {
-    return parseInt(arr.sort()[arr.length - 2]);
+    return numbers.sort((a, b) => a - b)[numbers.length - 2];
   }
 }
 
